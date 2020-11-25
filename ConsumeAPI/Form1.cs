941df5c781c2ece66db9361779950b1ef4504b66
Shengyin Li <sanyeeli15@gmail.com>
2020-11-26 07:35:59 +0800

    添加项目文件。

---
+++ ConsumeAPI/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsumeAPI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.DataSource = Enum.GetValues(typeof(httpVerb));
+
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            RestClient rClient = new RestClient();
+            rClient.endPoint = textBox1.Text;
+            rClient.httpMethod = (httpVerb)comboBox1.SelectedItem;
+
+            textBox2.Text = rClient.makeRequest();
+        }
+    }
+}
---
+++ ConsumeAPI/RestClient.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumeAPI
+{
+    public enum httpVerb
+    {
+        Get,
+        Post,
+        Put,
+        Delete
+    }
+
+    class RestClient
+    {
+        public string endPoint { get; set; }
+        public httpVerb httpMethod { get; set; }
+
+        public RestClient()
+        {
+            endPoint = string.Empty;
+            httpMethod = httpVerb.Get;
+        }
+        
+        public string makeRequest()
+        {
+            string strResponseValue = string.Empty;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(endPoint);
+            request.Method = httpMethod.ToString();
+
+
+            using(HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new ApplicationException("error code: " + response.StatusCode);
+                }
+
+                // Process the response stream ... (could be JSON, XML, or HTML etc...)
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    if (responseStream != null)
+                    {
+                        using(StreamReader reader=new StreamReader(responseStream))
+                        {
+                            strResponseValue = reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return strResponseValue;
+        }
+
+    }
+}
